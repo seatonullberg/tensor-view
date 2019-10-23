6311ed711c1eebe0c4152d542d633a74cd61c01b
Seaton Ullberg <seatonullberg@gmail.com>
2019-10-23 00:05:50 -0400

    framework for the gui, no internals yet

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/seatonullberg/tensor-view
+
+go 1.13
+
+require (
+	github.com/g3n/engine v0.1.1-0.20191001141548-13e8582dadaa
+	github.com/g3n/g3nd v0.1.0 // indirect
+	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
+	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
+	gopkg.in/yaml.v2 v2.2.4 // indirect
+)
---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"github.com/g3n/engine/app"
+	"github.com/g3n/engine/camera"
+	"github.com/g3n/engine/core"
+	"github.com/g3n/engine/geometry"
+	"github.com/g3n/engine/gls"
+	"github.com/g3n/engine/graphic"
+	"github.com/g3n/engine/gui"
+	"github.com/g3n/engine/light"
+	"github.com/g3n/engine/material"
+	"github.com/g3n/engine/math32"
+	"github.com/g3n/engine/renderer"
+	"github.com/g3n/engine/util/helper"
+	"github.com/g3n/engine/window"
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	// Create application and scene
+	a := app.App()
+	scene := core.NewNode()
+
+	// Set the scene to be managed by the gui manager
+	gui.Manager().Set(scene)
+
+	// Create perspective camera
+	cam := camera.New(1)
+	cam.SetPosition(0, 0, 3)
+	scene.Add(cam)
+
+	// Set up orbit control for the camera
+	camera.NewOrbitControl(cam)
+
+	// Set up callback to update viewport and camera aspect ratio when the window is resized
+	onResize := func(evname string, ev interface{}) {
+		// Get framebuffer size and update viewport accordingly
+		width, height := a.GetSize()
+		a.Gls().Viewport(0, 0, int32(width), int32(height))
+		// Update the camera's aspect ratio
+		cam.SetAspect(float32(width) / float32(height))
+	}
+	a.Subscribe(window.OnWindowSize, onResize)
+	onResize("", nil)
+
+	// Create a blue torus and add it to the scene
+	geom := geometry.NewTorus(1, .4, 12, 32, math32.Pi*2)
+	mat := material.NewStandard(math32.NewColor("DarkBlue"))
+	mesh := graphic.NewMesh(geom, mat)
+	scene.Add(mesh)
+
+	// Testing my stuff
+	var editFields []*gui.Edit
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			var s string
+			if i == j {
+				s = "1" 
+			} else {
+				s = "0"
+			}
+			ed := gui.NewEdit(40, "")
+			ed.SetText(s)
+			ed.SetFontSize(16)
+			ed.SetPosition(float32(i*50 + 5), float32(j*30 + 5))
+			editFields = append(editFields, ed)
+			scene.Add(ed)
+		}
+	}
+	btn := gui.NewButton("Update")
+	btn.SetPosition(47.5, 100)
+	btn.SetSize(50, 20)
+	btn.Subscribe(gui.OnClick, func(name string, ev interface{}) {
+		fmt.Println(editFields[0].Text())
+	})
+	scene.Add(btn)
+
+
+	// Create and add lights to the scene
+	//scene.Add(light.NewAmbient(&math32.Color{1.0, 1.0, 1.0}, 0.8))
+	//pointLight := light.NewPoint(&math32.Color{1, 1, 1}, 5.0)
+	scene.Add(light.NewAmbient(math32.NewColor("white"), 0.8))
+	pointLight := light.NewPoint(math32.NewColor("white"), 5.0)
+	pointLight.SetPosition(1, 0, 2)
+	scene.Add(pointLight)
+
+	// Create and add an axis helper to the scene
+	scene.Add(helper.NewAxes(0.5))
+
+	// Set background color to gray
+	a.Gls().ClearColor(0.5, 0.5, 0.5, 1.0)
+
+	// Run the application
+	a.Run(func(renderer *renderer.Renderer, deltaTime time.Duration) {
+		a.Gls().Clear(gls.DEPTH_BUFFER_BIT | gls.STENCIL_BUFFER_BIT | gls.COLOR_BUFFER_BIT)
+		renderer.Render(scene, cam)
+	})
+}
